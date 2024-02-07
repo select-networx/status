24dc028fe04c09b838d51ed6d4bf54f6cb43a1d6
Terry Kernan <tkernan@safetynetaccess.com>
2024-02-07 23:23:03 +0000

    image height

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
       <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="w-full h-40 absolute [background:#616362]">
         <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80">
-          <Image src="https://selectnetworx.com/wp-content/uploads/2023/06/selectnetworx.png" height={130} className="w-100 h-20" alt="System Status" />
+          <Image src="https://selectnetworx.com/wp-content/uploads/2023/06/selectnetworx.png" height={200} className="w-100 h-20" alt="System Status" />
         </div>
       </div>
       <div className='mt-20 w-full absolute overflow-scroll	'>
